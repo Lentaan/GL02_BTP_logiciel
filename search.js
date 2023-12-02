3b47a871fbc657f82732e826eb9dbbf15e610e95
pinilan <ilan.pin@utt.fr>
2023-12-02 18:40:55 +0100

    ajout de la fonction searchQuestion dans search

--- search.js
+++ search.js
@@ -1,2 +1,26 @@
 const parser = require('./parser.js');
+const readlineSync = require('readline-sync');
 List_question = new parser();
+
+// fonction qui permet à l'enseignant de rechercher une question ou la choisir dans une liste de questions
+function searchQuestion(){
+    // récupération de la question saisie par l'enseignant sur le terminal à l'aide de la fonction question de la bibliothèque readline-sync
+    let question = readlineSync.question('Entrez votre question : ');
+    // variable booléenne qui va nous permettre de savoir si la question a été trouvée ou non
+    let  questionFound = false;
+    // initialisation de l'indice de parcours de la liste de questions
+    let i = 0;
+    // création d'une boucle permmettant de parcourir les questions tout en recherchant la question saisie par l'enseignant
+    while(i<List_question.length && !questionTrouvee){
+        // utilisation d'une instruction conditionnelle afin de savoir si la question ou le mot sasisi par l'enseignant est contenu dans la liste de questions
+        if(List_question[i].name.includes(question)){
+            questionFound = true;
+            console.log(`La saisie ${question} a été trouvée, voici l'intitulé  de la question et sa réponse : \n ${List_question[i].name} \n ${List_question[i].answer}`);
+        }
+        i++;
+    }
+    // si la question n'est pas trouvée, on affiche un message d'erreur
+    if(!questionTrouvee){
+        console.log(`La question ${question} n'a pas été trouvée`);
+    }
+}

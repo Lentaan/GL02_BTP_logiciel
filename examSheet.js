ea05735af07d53cd2ce41a4eff7e9e7160c0c776
pinilan <ilan.pin@utt.fr>
2023-12-03 18:40:23 +0100

    ajout d'une fonction listQuestionExam dans examSheet

--- examSheet.js
+++ examSheet.js
@@ -13,7 +13,33 @@ const search = function(){
         return result.name;
     // sinon on retourne null
     } else {
-        console.log('Il y a une erreur dans votre saisie, veuillez recommencer');
         return null;
     }
+}
+
+// fonction qui retourne une liste regroupant les questions saisies par l'enseignant en se servant de la fonction search
+const listQuestionExam = function(){
+    // création de liste qui va contenir les questions
+    let questions_exam = Array();
+    // création d'une variable question
+    let question;
+    // création d'une variable qui va détermniner si l'enseignant veut ajouter une question
+    let insert = readlineSync.question('Voulez-vous ajouter une question (oui/non) : ');
+    // tant que l'enseignant veut ajouter des questions, elles seront ajoutées à la liste
+    do{
+        // utilisation de la fonction search
+        question = search();
+        // si la question a été trouvée, elle est ajoutée à la liste
+        if(question != null){
+            questions_exam.push(question);
+            console.log('La question a été ajoutée \n')
+        // sinon elle n'est pas ajoutée
+        } else {
+            console.log('La question n\'a pas été trouvée, elle n\'a pas été ajoutée \n')
+        }
+        // on demande à l'enseignant s'il veut ajouter une question
+        insert = readlineSync.question('Voulez-vous ajouter une question (oui/non) : ');
+    } while(insert.toLowerCase() == 'oui');
+    // on retourne la liste de questions
+    return questions_exam;
 }

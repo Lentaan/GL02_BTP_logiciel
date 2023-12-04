a0ed95b45b3555c2c2b4114c0d00af0a1c4ab421
eFuselier <fuselieremma08@gmail.com>
2023-12-04 23:26:35 +0100

    debut de la SPEC5

--- SPEC5.js
+++ SPEC5.js
@@ -1,6 +1,19 @@
   // Realiser la simulation d'un examen: 
   //l'étudiant veut s'entrainer à passer un examen pour tester ses connaissances et 
   //veut un bilan de ses reponses 
-  // 
+  // l'étudiant doit rentrer un fichier d'examen qu'il a selectionne et (cf Florian) on va comparer ses réponses au fichier reponse
+  // et on va lui faire un retour sur ses réponses
     //simulation
-    
+
+  const parser = require('./parser');
+  const parserExam = require ('./parserExam');
+  List_examen = parserExam();
+
+
+//insert
+  .command("insert", "Insert a file of answer")
+  .argument('<new file>', 'rentrer un fichier GIFT avec des réponses uniquement')
+   
+    const insert = function(){
+      
+    }

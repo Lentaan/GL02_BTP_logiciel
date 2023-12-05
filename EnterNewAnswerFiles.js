4bb29f43656f7e355b0299da2a1b5af112b2389b
eFuselier <fuselieremma08@gmail.com>
2023-12-05 22:41:46 +0100

    Avance de la passation d'examen

--- EnterNewAnswerFiles.js
+++ EnterNewAnswerFiles.js
@@ -1,34 +1,56 @@
   // Realiser la simulation d'un examen: 
   //l'étudiant veut s'entrainer à passer un examen pour tester ses connaissances et 
   //veut un bilan de ses reponses 
-  // l'étudiant doit rentrer un fichier d'examen qu'il a selectionne et (cf Florian) on va comparer ses réponses au fichier reponse
-  // et on va lui faire un retour sur ses réponses
-    //simulation
-  const { error } = require('console');
-  const QUESTION = require ('./QUESTION.js');
-const fs = require('fs');
 
- //// la fonction insertAnswerFile permet à l'utilisateur d'insérer un fichier réponse d'examen
+const { error } = require('console');
+const QUESTION = require ('./QUESTION.js');
+const fs = require('fs');
+List_exam = parserExam();
+
+
+ //// ******la fonction insertAnswerFile permet à l'utilisateur de créer et d'insérer un fichier réponse d'examen*****////
 //insertAnswerfile
-const insertAnswerFile = function() {
-  // création de la variable 
-  var createNewAnswerFile = 'Réponses de l etudiant à un examen';
+function AskExam () {
+  // Trouver l'examen que l'etudiant veut passer
+  let examen =fs.readFileSync.question('Quel examen souhaitez-vous préparez?');
+  let resultat = List_exam.find((e) => e.name.includes(examen));
+  if(resultat){
+    return resultat.name;
+  }else{
+    return null;
+  }
+}
+
+  // création de la fonction pour créer un fichier de reponse 
+  function createNewAnswerFile () { 
   // Le fichier est crée et peut-etre modifié à partir de la grâce à fs.appendFile
-    fs.appendFile('NewFileAnswer.gift', answer,'utf8', function(err) {
+    fs.appendFile(`${fileName}.gift`, answers,'utf8', function(err) {
+      // en cas d'erreur l'utilisateur le sait 
       if(err){
        console.log("erreur");
        return;
       }
 
       console.log('Fichier crée. Vous pouvez ecrire vos réponses');
-    
-    })
+      })
     
     };
 
 //comparer le fichier de l'etudiant avec le fichier de l'examen passé cf Florian
+async function compareExams(fileName,resultat ) {
+  // Verification des réponses
+  const commonAnswer = fileName.filter(answer => resultat.includes(anwser));
+  return commonAnswer;
+  return ;
 
-// faire un compte rendu de la passation de l'examen
+}
+
+
+
+
+
+module.exports = {compareExams, AskExam, createNewAnswerFile };
+
     
 
     

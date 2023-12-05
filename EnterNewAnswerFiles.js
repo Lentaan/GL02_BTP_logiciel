3ae3cb9623fe81daae1c6dc4a6c77d55b061c1d2
eFuselier <fuselieremma08@gmail.com>
2023-12-05 21:51:50 +0100

    debut de EnterNewAnswerFiles

--- EnterNewAnswerFiles.js
+++ EnterNewAnswerFiles.js
@@ -4,14 +4,35 @@
   // l'étudiant doit rentrer un fichier d'examen qu'il a selectionne et (cf Florian) on va comparer ses réponses au fichier reponse
   // et on va lui faire un retour sur ses réponses
     //simulation
-  const fs = require('fs')
+  const { error } = require('console');
+  const QUESTION = require ('./QUESTION.js');
+const fs = require('fs');
 
  //// la fonction insertAnswerFile permet à l'utilisateur d'insérer un fichier réponse d'examen
 //insertAnswerfile
-function insertAnswerFile() {
-  
-  
-}
+const insertAnswerFile = function() {
+  // création de la variable 
+  var createNewAnswerFile = 'Réponses de l etudiant à un examen';
+  // Le fichier est crée et peut-etre modifié à partir de la grâce à fs.appendFile
+    fs.appendFile('NewFileAnswer.gift', answer,'utf8', function(err) {
+      if(err){
+       console.log("erreur");
+       return;
+      }
 
+      console.log('Fichier crée. Vous pouvez ecrire vos réponses');
+    
+    })
+    
+    };
+
+//comparer le fichier de l'etudiant avec le fichier de l'examen passé cf Florian
+
+// faire un compte rendu de la passation de l'examen
+    
+
+    
+
+    
     
     

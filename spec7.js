902f7bce59a929ab68cc92c25a3fc4885cadbaea
panchotf <florianp0124@gmail.com>
2023-12-06 19:54:10 +0100

    Ajout des questions, du choix de la base de donnée mais comparaison marche pas

--- spec7.js
+++ spec7.js
@@ -1,18 +1,118 @@
-const parsfile = require('./parsfile');
-//const parser = require('./parser.js');//parser pour tous les exams
+const parsfile = require('./parsfile'); //appel du parser
 const fs = require('fs');
 const readlineSync = require('readline-sync');
 
+let exam1;
+let exam2;
+
+//=============================choisir un fichier perso=========================================//
+
+// Récupérer la liste des fichiers disponibles dans le répertoire (inutile)
+const examfile = fs.readdirSync('./exam'); // Le chemin du répertoire où se trouvent ses propres fichiers
+
+// Afficher les fichiers disponibles à l'utilisateur
+console.log('Fichiers disponibles :');
+examfile.forEach((file, index) => {
+    console.log(`${index + 1}. ${file}`);
+});
+
+// Demander à l'utilisateur de choisir le premier fichier
+let choix = readlineSync.questionInt('Choisissez un premier fichier en entrant son numéro : ');
+
+// Vérifier si le choix est valide
+if (choix > 0 && choix <= examfile.length) {
+    exam1 = examfile[choix - 1];
+    console.log(`Vous avez choisi : ${exam1}\n`);
+
+} else {
+    console.log('Choix invalide');
+}
+
+
+//========================================demander a quoi on souhaite le comparer====================================//
+
+
+// Demander à l'utilisateur de choisir le premier fichier
+choix = readlineSync.questionInt('A quoi souhaitez-vous comparer ce fichier ?\n 1. Vos fichiers personnals \n 2. La base de donnée\n');
+
+// Vérifier si le choix est valide
+if (choix > 0 && choix <= 2) {
+    console.log(`Vous avez choisi : ${choix}\n`);
+
+} else {
+    console.log('Choix invalide');
+}
+//===================================choisir le 2eme fichier==============================//
+if(choix === 1){
+    // Afficher les fichiers disponibles à l'utilisateur
+    console.log('Fichiers disponibles :');
+    examfile.forEach((file, index) => {
+        console.log(`${index + 1}. ${file}`);
+    });
+
+// Demander à l'utilisateur de choisir le premier fichier
+    let choix = readlineSync.questionInt('Choisissez un premier fichier en entrant son numéro : ');
+
+// Vérifier si le choix est valide
+    if (choix > 0 && choix <= examfile.length) {
+        exam2 = examfile[choix - 1];
+        console.log(`Vous avez choisi : ${exam1}\n`);
+
+    } else {
+        console.log('Choix invalide');
+    }
+
+}else{
+    // Récupérer la liste des fichiers disponibles dans le répertoire
+    const files = fs.readdirSync('./SujetB_data'); // Le chemin du répertoire où se trouvent tes fichiers
+
+// Afficher les fichiers disponibles à l'utilisateur
+    console.log('Fichiers disponibles :');
+    files.forEach((file, index) => {
+        console.log(`${index + 1}. ${file}`);
+    });
+
+    choix = readlineSync.questionInt('Choisissez un premier fichier en entrant son numéro : ');
+
+// Vérifier si le choix est valide
+    if (choix > 0 && choix <= files.length) {
+        exam2 = files[choix - 1];
+        console.log(`Vous avez choisi : ${exam1}`);
+
+    } else {
+        console.log('Choix invalide');
+    }
+}
+
+//==============================Parser les fichiers=================================//
+let questionExam1 = parsfile(exam1);
+//console.log(questionExam1);
+
+let questionExam2 = parsfile(exam2);
+//console.log(questionExam2);
+
+
+//=============================== Comparaison des questions des fichiers==================================//
+questionExam1.forEach((question1) => {
+    const matchedQuestion = questionExam2.find((question2) => {
+        return question1.name === question2.name && question1.answer === question2.answer && question1.type === question2.type;
+    });
+
+    if (matchedQuestion) {
+        console.log(`La question "${question1.name}" est identique dans les deux examens.`);
+    }
+});
+
+
+
+/*
 let exam1 = './exam/exam1.gift';
 let exam2 = './exam/exam2.gift';
 
 
 
-
-
-
 // Récupérer la liste des fichiers disponibles dans le répertoire (inutile)
-/*const files = fs.readdirSync('./SujetB_data'); // Le chemin du répertoire où se trouvent tes fichiers
+const files = fs.readdirSync('./SujetB_data'); // Le chemin du répertoire où se trouvent tes fichiers
 
 // Afficher les fichiers disponibles à l'utilisateur
 console.log('Fichiers disponibles :');
@@ -21,18 +121,17 @@ files.forEach((file, index) => {
 });
 
 // Demander à l'utilisateur de choisir le premier fichier
-const choix1 = readlineSync.questionInt('Choisissez un premier fichier en entrant son numéro : ');
+let choix = readlineSync.questionInt('Choisissez un premier fichier en entrant son numéro : ');
 
 // Vérifier si le choix est valide
-if (choix1 > 0 && choix1 <= files.length) {
-    exam1 = files[choix1 - 1];
+if (choix1 > 0 && choix <= files.length) {
+    exam1 = files[choix - 1];
     console.log(`Vous avez choisi : ${exam1}`);
-    console.log('coucou',exam1);
 
 } else {
     console.log('Choix invalide');
 }
-console.log('coucou',exam1);
+
 // Demander à l'utilisateur de choisir le deuxième fichier
 const choix2 = readlineSync.questionInt('Choisissez un deuxieme fichier en entrant son numéro : ');
 
@@ -47,103 +146,15 @@ if (choix2 > 0 && choix2 <= files.length) {
 } else {
     console.log('Choix invalide');
 }
-*/
+
 
 
 let questionExam1 = parsfile(exam1);
-console.log(questionExam1);
+//console.log(questionExam1);
 
 let questionExam2 = parsfile('./exam/exam2.gift');
-console.log(questionExam2);
-
-// Comparaison des questions
-questionExam1.forEach((question1) => {
-    const matchedQuestion = questionExam2.find((question2) => {
-        return question1.name === question2.name && question1.answer === question2.answer && question1.type === question2.type;
-    });
-
-    if (matchedQuestion) {
-        console.log(`La question "${question1.name}" est identique dans les deux examens.`);
-    }
-});
+//console.log(questionExam2);
 
 
 
-/*list_question = parser();
-console.log(exam1);*/
-
-
-
-/*function listQuestionsFile(exam) {
-    let questionsExam = list_question.filter(question => question.nomFichier === exam);
-    return questionsExam;
-}
-
-
-let questionExam1 = listQuestionsFile(exam1);
-console.log(questionExam1);*/
-
-/*// Filtrer les questions pour afficher uniquement celles ayant le même nomFichier que exam1
-const questionsExam1 = list_question.filter(question => question.nomFichier === exam1);
-
-// Afficher les questions de exam1
-console.log('Questions de', exam1);
-questionsExam1.forEach(question => {
-    console.log(question.toString());
-});*/
-
-
-
-
-
-//console.log(list_question[choix1-1]);
-
-
-
-
-
-
-
-/*const readline= require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-*/
-
-
-//On recupere les questions des examens
-//exam1 = parserExam('./exam1.gift');
-//exam2 = parserExam('./exam2.gift');
-
-//console.log(list_question);
-//console.log(exam2);
-
-
-/*
-// Fonction pour comparer les questions de deux examens
-function compareExams(exam1, exam2) {
-    // Comparaison basique : vérification des questions identiques
-    const commonQuestions = exam1.filter(question => exam2.includes(question));
-    return commonQuestions;
-}
-
-
-
-
-        // Extraire les questions du second examen
-        const exam2Questions = extractQuestions(data2);
-
-        // Comparer les questions des deux examens
-        const commonQuestions = compareExams(exam1Questions, exam2Questions);
-
-        // Afficher les questions communes
-        console.log('Questions communes entre les deux examens :');
-        commonQuestions.forEach((question, index) => {
-            console.log(`${index + 1}. ${question}`);
-        });
-    });
-});
 */

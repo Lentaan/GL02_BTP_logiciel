0bd36d423398b0d8300554dd66f37a024434bc81
panchotf <florianp0124@gmail.com>
2023-12-06 21:24:10 +0100

    spec7 compare deux fichiers de meme ou de différentes base de données et trouve les questions communes s'il y a

--- spec7.js
+++ spec7.js
@@ -6,6 +6,7 @@ const path = require('path');
 let exam1;
 let exam2;
 
+
 //=============================choisir un fichier perso=========================================//
 
 // Récupérer la liste des fichiers disponibles dans le répertoire (inutile)
@@ -92,74 +93,25 @@ console.log(`./${dirPath}/${exam1}`);*/
 
 
 let questionExam1 = parsfile(exam1);
-console.log(questionExam1);
+//console.log(questionExam1);
 
 let questionExam2 = parsfile(exam2);
-console.log(questionExam2);
+//console.log(questionExam2);
 
 //=============================== Comparaison des questions des fichiers==================================//
-/*questionExam1.forEach((question1) => {
+let bool = false;
+
+questionExam1.forEach((question1) => {
     const matchedQuestion = questionExam2.find((question2) => {
         return question1.name === question2.name && question1.answer === question2.answer && question1.type === question2.type;
     });
-
     if (matchedQuestion) {
         console.log(`La question "${question1.name}" est identique dans les deux examens.`);
+        bool = true;
     }
 });
-*/
 
-
-/*
-let exam1 = './exam/exam1.gift';
-let exam2 = './exam/exam2.gift';
-
-
-
-// Récupérer la liste des fichiers disponibles dans le répertoire (inutile)
-const files = fs.readdirSync('./SujetB_data'); // Le chemin du répertoire où se trouvent tes fichiers
-
-// Afficher les fichiers disponibles à l'utilisateur
-console.log('Fichiers disponibles :');
-files.forEach((file, index) => {
-    console.log(`${index + 1}. ${file}`);
-});
-
-// Demander à l'utilisateur de choisir le premier fichier
-let choix = readlineSync.questionInt('Choisissez un premier fichier en entrant son numéro : ');
-
-// Vérifier si le choix est valide
-if (choix1 > 0 && choix <= files.length) {
-    exam1 = files[choix - 1];
-    console.log(`Vous avez choisi : ${exam1}`);
-
-} else {
-    console.log('Choix invalide');
+if (!bool) {
+    console.log('Aucune question identique n\'a été trouvée dans les deux examens.');
 }
 
-// Demander à l'utilisateur de choisir le deuxième fichier
-const choix2 = readlineSync.questionInt('Choisissez un deuxieme fichier en entrant son numéro : ');
-
-// Vérifier si le choix est valide
-if (choix2 > 0 && choix2 <= files.length) {
-    exam2 = files[choix2 - 1];
-    console.log(`Vous avez choisi : ${exam2}`);
-
-
-    // Maintenant tu peux utiliser le fichier choisi comme tu le souhaites
-    // Par exemple : fs.readFileSync(fichierChoisi, 'utf-8');
-} else {
-    console.log('Choix invalide');
-}
-
-
-
-let questionExam1 = parsfile(exam1);
-//console.log(questionExam1);
-
-let questionExam2 = parsfile('./exam/exam2.gift');
-//console.log(questionExam2);
-
-
-
-*/

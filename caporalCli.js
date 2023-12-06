4ccae3ef096b13cdc6ce44f4f7d1b7317b69ae4c
FredWantou <freddywantou@hotmail.com>
2023-12-06 19:54:47 +0100

    ajout de la specification 5

--- caporalCli.js
+++ caporalCli.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const colors = require('colors');
 //const VpfParser = require('./VpfParser.js');
 const parser = require('./parser');
+const parserExam = require('./parserExam');
 const listQuestion = parser();
 const readline = require('readline');
 const vg = require('vega');
@@ -33,6 +34,32 @@ cli
 				logger.info('fichier non existant ou chemin inacessible');
 			}
 			logger.info(`La question ${args.number} a ete ajoutee`);
+		})
+	})
+
+	//simulation de test 
+	.command('compareAnswer',"Compare les reponses d'un etudiant avec la correction d'un exam choisi")
+	.argument('<answer>',"fichier contenant les reponses d'un etudiant")
+	.argument('<exam>',"examen compose par l'etudiant")
+	.action(({args,logger,options}) => {
+		//reponse de l'etudiant
+		fs.readFile(args.answer,"utf-8", (err,data) =>{
+			if(err){
+				logger.info("Le fichier de reponse de l'etudiant est illisible")
+			}
+			answerStudent = data.split(',');
+			//reponse aux questions de l'examen
+			questionExam =parserExam(args.exam);
+			//note a l'examen
+			let note = 0;
+			//numero correspond au nulero de question
+			for(let numero = 0; numero < questionExam.length; numero++){
+				if(answerStudent[numero] === questionExam[numero].answer){
+					note++
+				}
+			}
+			note = note * 20/questionExam.length;
+			logger.info(note);
 		})
 	})
 				

e9f46284b47ee9350c846ab761e4bf476c04e574
FredWantou <freddywantou@hotmail.com>
2023-12-08 19:12:30 +0100

    ajout de la fonction de visualisation du type de question de toute la base de données

--- Visualizealldata.js
+++ Visualizealldata.js
@@ -6,41 +6,36 @@ const parserExam = require('./parserExam.js');
 const programme = require('@caporal/core').default;
 const SujetB_data = require ('SujetB_data');
 
-
-
-programme
-    .command('VisualizeAllData','Visualize by a graph all data')
-    .argument('<SujetB_data>', "All files use to make the graph")
-    .action( ({args,logger,options}) => {
-    // 
-        const filePaths = args.SujetB_data;
-        
-        // Fonction pour lire les données d'un fichier
+function visualizealldata(){
+    //on recupere les repertoires de travail
+    const filePaths = SujetB_data;
+    //on lit chaque fichier present dans le repertoire
         const readDataFromFile = (filePath) => {
-            const data = JSON.parse(fs.readFileSync(filePath,'UTF-8'));
+            const data = JSON.parse(fs.readFileSync(filePath, 'UTF-8'));
             return data;
         }
-        // Charger les données de chaque fichier
+        //on recupere les donnees des fichiers
         const datasets = filePaths.map(filePath => readDataFromFile(filePath));
-
-        // Combinez les jeux de données si nécessaire
+        //on combine les donnees des fichiers dans un unique
         const combinedData = datasets.reduce((acc, dataset) => acc.concat(dataset), []);
-        
-        // variable pour la visualisation Vega-Lite
-        var visualize ={
-            "data" : {"values": combinedData}, 
-            "mark" : "bar",
-            "encoding" : { x : { "field" : "type", "type" : "nominal", 
-                                "axis": {"title": "Types de questions"}
-                                },
-                        y : {"field" : "Nombre de questions","aggregate" : "count"}
-                            }
-                            }
-        //Afficher le graphique
-        console.log(visualize);
-                        
-                        })
-    
- //exportation de la fonction VisualizeAllData
-programme.run(process.argv.slice(2));
+        //on visualise les donnees
+        const visualize = {
+            "data": { "values": combinedData },
+            "mark": "bar",
+            "encoding": {
+                "x": {
+                    "field": "type",  // Utiliser le bon nom d'attribut
+                    "type": "nominal",
+                    "axis": { "title": "Types de questions" }
+                },
+                "y": {
+                    "field": "type",  // Utiliser le bon nom d'attribut ou un autre attribut existant
+                    "aggregate": "count"
+                }
+            }
+        }
 
+        console.log(visualize);
+
+}
+module.exports = visualizealldata;
